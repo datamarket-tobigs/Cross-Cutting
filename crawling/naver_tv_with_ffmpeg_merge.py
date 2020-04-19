870a66c3a1e9cc8dba7120d64482242d442bc4f6
dizwe <dizwe2716@gmail.com>
2020-04-19 12:17:04 +0900

    RESOLVE naver_tv 영상 중간중간 끊기는 문제 해결 ffmpeg splicing 고려 안해서 구획 끝과 시작이 겹쳤던 문제 해결함 moviepy는 그런 기능이 없어서 사용 못할 듯

--- crawling/naver_tv_with_ffmpeg_merge.py
+++ crawling/naver_tv_with_ffmpeg_merge.py
@@ -1,16 +1,21 @@
 #%%
 import requests
+contents_length = 1
 
-for idx in range(1, 21):
-    # url = f'https://b01-kr-naver-vod.pstatic.net/navertv/c/read/v2/VOD_ALPHA/TVCAST_2017_01_20_3/261080qWM2BDXyzWWsdTIOMhjA_rmcvideo_1080P_1920_5120_192-00000{idx}.ts?_lsu_sa_=6025a5f6c15c6fd60ed4658364e50eb8ae9f3d18f3018f953f07cac0f79a36a55c256a316e35330d70503652d73fdba0dae633342219a51676e337002dcb4e7a5ef8e1bab168d1bc47d8940664b493c3bf4ef7671332cb8ba70eb6d2cb797cffd0f2b3289c6e197c5e098e6f866d5ba17e6c319aedc766a03d5cd9689c5537b9'
-    # url = f'https://naver-mbc-h.smartmediarep.com/smc/naver/adaptive/eng/M01_TZ202004090001/2f6d62632f4174746163682f4d42432f564944454f2f543730303036472f434c49502f636c69705f545a3230323030343039303030315f32303230303430393030303235342e736d696c/0-0-0/content_5120000_{idx}.ts?solexpire=1586438868&solpathlen=197&soltoken=c30d6e412cd032b378dd07dfdb27d413&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=dcb6a588-bbba-4462-96de-e60e9434006a'
-    url = f'https://naver-mbc-h.smartmediarep.com/smc/naver/adaptive/eng/M01_TZ202004090011/2f6d62632f4174746163682f4d42432f564944454f2f543730303036472f434c49502f636c69705f545a3230323030343039303031315f32303230303430393030333833302e736d696c/0-0-0/content_5120000_{idx}.ts?solexpire=1586479670&solpathlen=197&soltoken=052622bb2c1d995fb0d307b508d44c6f&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=cc588313-42bc-465f-9b5f-e5d7832cdd17'
+# 전체 길이 찾을 수 있게
+while True:
+    # url = f'https://naver-sbs-c.smartmediarep.com/smc/naver/adaptive/eng/S01_22000335770/2f7362732f75706c6f61642f3232302f30303333352f32323030303333353737306b5f76322e736d696c/0-0-0/content_2048000_{contents_length}.ts?solexpire=1587048639&solpathlen=130&soltoken=dbee156e41e74922cb86323eca442d26&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=22ce80c2-c758-4fd6-a855-bb4475bc3dd9'
+    url = f'https://naver-sbs-c.smartmediarep.com/smc/naver/adaptive/eng/S01_22000335770/2f7362732f75706c6f61642f3232302f30303333352f32323030303333353737306b5f76322e736d696c/0-0-0/content_5120000_{contents_length}.ts?solexpire=1587308911&solpathlen=130&soltoken=cd685af8f4a67c9c2f20741349420889&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=2d586286-dd4a-4019-920d-3ee81cb20bea'
     r = requests.get(url, allow_redirects=True)
-    open(f'contents_{idx}.ts', 'wb').write(r.content)
-
+    if r.status_code==404:
+        contents_length = contents_length -1 #애는 쓸모 없는 애니까
+        break
+    open(f'contents_{contents_length}.ts', 'wb').write(r.content)
+    contents_length = contents_length + 1
+    
 # %%
 # https://trac.ffmpeg.org/wiki/Concatenate
 import subprocess
-subprocess.getoutput('for f in ./*.ts; do echo "file \'$f\'" >> mylist.txt; done')
-# %%
-subprocess.getoutput('ffmpeg -f concat -safe 0 -i mylist.txt -c copy mergedfile.ts')
+concat_files = [f'contents_{i}.ts'for i in range(1,contents_length+1)]
+concat_str = '|'.join(concat_files)
+subprocess.getoutput(f'ffmpeg -i "concat:{concat_str}" -c copy output.ts')
