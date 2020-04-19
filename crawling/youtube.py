71ce4ab5dd7ec86e433bb403689d93df87865207
dizwe <dizwe2716@gmail.com>
2020-04-19 12:58:59 +0900

    RESOLVE youtube crawl 소리 없는 720p에 mp3에 붙이기 성공 moviepy가 안됨.. ffmpeg로 수동으로 붙임

--- crawling/youtube.py
+++ crawling/youtube.py
@@ -4,8 +4,9 @@ import os
 
 YTID = '_ouI0sPnUpA'
 
-#%%
-### get_data
+# %%
+######### 음성 붙이기 시도
+# 720p는 음성이 없는데 붙이는 시도 했는데 계속 오류나서 해결 중..
 from pytube import YouTube
 import moviepy.editor as mp # 음성 붙이기 위함
 
@@ -13,42 +14,32 @@ import subprocess
 import os
 error_list = []
 current_dir = os.getcwd()
-
 try:
     yt = YouTube(f'https://www.youtube.com/watch?v={YTID}')
-    print(yt.streams.filter(file_extension='mp4',res='720p').all())
-    vids = yt.streams.filter(file_extension='mp4',res='720p').all() 
-    # [<Stream: itag="140" mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2">, <Stream: itag="249" mime_type="audio/webm" abr="50kbps" acodec="opus">, <Stream: itag="250" mime_type="audio/webm" abr="70kbps" acodec="opus">, <Stream: itag="251" mime_type="audio/webm" abr="160kbps" acodec="opus">]
-    vids[0].download(f'{current_dir}/720/')
+    # print(yt.streams.filter(file_extension='mp4',res='720p').all())
+    print(yt.streams.all())
+    # vids = yt.streams.filter(file_extension='mp4',res='720p').all() 
+    # vids_360 = yt.streams.filter(file_extension='mp4',res='360p').all() 
+    # # [<Stream: itag="140" mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2">, <Stream: itag="249" mime_type="audio/webm" abr="50kbps" acodec="opus">, <Stream: itag="250" mime_type="audio/webm" abr="70kbps" acodec="opus">, <Stream: itag="251" mime_type="audio/webm" abr="160kbps" acodec="opus">]
+    # vids[0].download(f'{current_dir}/720/')
     
-except Exception as ex:
-    print('에러가 발생 했습니다', ex)
-
-
-
-# %%
-######### 음성 붙이기 시도
-# 720p는 음성이 없는데 붙이는 시도 했는데 계속 오류나서 해결 중..
-try:
-    yt = YouTube(f'https://www.youtube.com/watch?v={YTID}')
-    print(yt.streams.filter(file_extension='mp4',res='720p').all())
-    vids = yt.streams.filter(file_extension='mp4',res='720p').all() 
-    vids_360 = yt.streams.filter(file_extension='mp4',res='360p').all() 
-    # [<Stream: itag="140" mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2">, <Stream: itag="249" mime_type="audio/webm" abr="50kbps" acodec="opus">, <Stream: itag="250" mime_type="audio/webm" abr="70kbps" acodec="opus">, <Stream: itag="251" mime_type="audio/webm" abr="160kbps" acodec="opus">]
-    vids[0].download(f'{current_dir}/720/')
-    
-    # 음성 붙이기
-    ### Overwrite 안됨.
-    vids_360[0].download(f'{current_dir}/360/')
-    subprocess.call(['ffmpeg','-ss','0','-i',
-        os.path.join(f'{current_dir}/360/',vids_360[0].default_filename),
-        os.path.join(f'{current_dir}/360/','a.mmp3')
-    ])
+    # # 음성 붙이기
+    # ### Overwrite 안됨.
+    # vids_360[0].download(f'{current_dir}/360/')
+    # subprocess.call(['ffmpeg','-ss','0','-i',
+    #     os.path.join(f'{current_dir}/360/',vids_360[0].default_filename),
+    #     os.path.join(f'{current_dir}/360/','a.mp3')
+    # ])
     # ffmpeg -ss (start time) -i (direct video link) -t (duration needed) -c:v copy -c:a copy (destination file
 except Exception as ex:
     print('에러가 발생 했습니다', ex)
 # ffmpeg -ss 10 -i kk.mp4 -t 30 a.mp3
 # ffmpeg -i guitar1.mp4 guitar1.mp3
+
+# %%
+import subprocess
+subprocess.getoutput(f'ffmpeg -i {current_dir}/720/k.mp4 -i {current_dir}/360/a.mp3 -c copy output.mp4')
+
 #%%
 ## Trial 720p 음악 없는거 오디오넣기 -> 안됨 ㅜㅜ
 # https://github.com/Zulko/moviepy/issues/213
