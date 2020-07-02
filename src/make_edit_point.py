31fa2dcdccc38dc3e5709b1341ba831931b7d1ea
minjoong507 <albert507@naver.com>
2020-07-03 00:59:09 +0900

    유사도 뽑기 및 다음영상, 편집시점 계산

--- src/make_edit_point.py
+++ src/make_edit_point.py
@@ -1,15 +1,48 @@
-from src.fastRCNN_model import fast_RCNN_model
+import numpy as np
+from fastRCNN_model import fast_RCNN_model
 
-img_dir = "../img/"
-subclip_dir = [str(i*10)+"-"+str((i+1)*10)+"/" for i in range(23)]
-video_num = [str(i) + " video " for i in [0, 1, 4, 5]]
-frame_num = [str(i*10)+".png" for i in range(27)]
-file = img_dir + subclip_dir[0] + video_num[0] + frame_num[0]
+class make_edit_point():
 
-model = fast_RCNN_model()
+    def __init__(self, start_video):
+        self.img_dir = "../img/"
+        self.subclip_dir = [str(i * 10) + "-" + str((i + 1) * 10) + "/" for i in range(23)]
+        self.video_num = [str(i) + " video " for i in [0, 1, 4, 5]]
+        self.frame_num = [str(i * 30) + ".png" for i in range(9)]
+        self.model = fast_RCNN_model()
+        self.next_start = start_video
+        self.result = []
 
-model.write_file_name(file_name=file)
-box_id, box_score, box_val = model.run_model()
+    def calculate_time_video(self):
+        for period in self.subclip_dir:
+            video_point = []
+            start = self.next_start
+            for f_num in self.frame_num:
+                per_frame = []
+                for v_num in self.video_num[:4]:
+                    file_name = self.img_dir + period + v_num + f_num
+                    # print(file_name)
+                    self.model.write_file_name(file_name=file_name)
+                    box_id, box_score, box_val = self.model.run_model()
+                    point = np.sum(box_val) / 6000
+                    per_frame.append(point.asnumpy()[0])
+                video_point.append(per_frame)
 
-box_val = box_val[0]
-print(box_val.getitem)
+            frame_similarity = []
+            for i in range(len(video_point)):
+                temp = []
+                for j in range(len(video_point[i])):
+                    if j != start:
+                        temp.append(abs(video_point[i][start] - video_point[i][j]))
+                frame_similarity.append(temp)
+
+            edit_point = frame_similarity.index(min(frame_similarity))
+            next_video = frame_similarity[edit_point].index(min(frame_similarity[edit_point]))
+            self.next_start = next_video
+            # print("edit point: " + str(edit_point))
+            # print("next_video: " + str(next_start))
+            # print("____")
+            r = [edit_point, next_video]
+            self.result.append(r)
+
+    def return_result(self):
+        return self.result
