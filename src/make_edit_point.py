805a38f4c6981b4374a27919a187f865e2a00fd0
minjoong507 <albert507@naver.com>
2020-07-02 23:13:55 +0900

    .,

--- src/make_edit_point.py
+++ src/make_edit_point.py
@@ -1,9 +1,15 @@
 from src.fastRCNN_model import fast_RCNN_model
 
+img_dir = "../img/"
+subclip_dir = [str(i*10)+"-"+str((i+1)*10)+"/" for i in range(23)]
+video_num = [str(i) + " video " for i in [0, 1, 4, 5]]
+frame_num = [str(i*10)+".png" for i in range(27)]
+file = img_dir + subclip_dir[0] + video_num[0] + frame_num[0]
+
 model = fast_RCNN_model()
-file = "../img/0-10/5 video 210.png"
+
 model.write_file_name(file_name=file)
-a, b, c = model.run_model()
-print(a)
-print(b)
-print(c)
+box_id, box_score, box_val = model.run_model()
+
+box_val = box_val[0]
+print(box_val.getitem)
