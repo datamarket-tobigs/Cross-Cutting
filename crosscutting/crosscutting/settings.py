91c8f0fb9da82989a0e331282892743c45dd2a01
myungsegyo <myungsekyo@gmail.com>
2020-07-07 19:10:24 +0900

    update template dir

--- crosscutting/crosscutting/settings.py
+++ crosscutting/crosscutting/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'crosscutting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
