a2029b4c86672de05c31a8f67ffa8a8759bf4545
minjoong507 <albert507@naver.com>
2020-07-11 13:43:00 +0900

    수정

--- pose_detection/make_edit_point.py
+++ pose_detection/make_edit_point.py
@@ -2,11 +2,10 @@ import numpy as np
 from fastRCNN_model import fast_RCNN_model
 
 class make_edit_point():
-
     def __init__(self, start_video):
-        self.img_dir = "../img/"
+        self.img_dir = "img/"
         self.subclip_dir = [str(i * 10) + "-" + str((i + 1) * 10) + "/" for i in range(23)]
-        self.video_num = [str(i) + " video " for i in [0, 1, 4, 5]]
+        self.video_num = [str(i) + " video " for i in range(1,10)]
         self.frame_num = [str(i * 30) + ".png" for i in range(9)]
         self.model = fast_RCNN_model()
         self.next_start = start_video
@@ -18,9 +17,9 @@ class make_edit_point():
             start = self.next_start
             for f_num in self.frame_num:
                 per_frame = []
-                for v_num in self.video_num[:4]:
+                for v_num in self.video_num:
                     file_name = self.img_dir + period + v_num + f_num
-                    # print(file_name)
+                    print(file_name)
                     self.model.write_file_name(file_name=file_name)
                     box_id, box_score, box_val = self.model.run_model()
                     point = np.sum(box_val) / 6000
@@ -41,11 +40,15 @@ class make_edit_point():
             edit_point = frame_similarity.index(min(frame_similarity))
             next_video = frame_similarity[edit_point].index(min(frame_similarity[edit_point]))
             self.next_start = next_video
-            # print("edit point: " + str(edit_point))
-            # print("next_video: " + str(next_start))
-            # print("____")
+            print("edit point: " + str(edit_point))
+            print("next_video: " + str(self.next_start))
+            print("____")
             r = [edit_point, next_video]
             self.result.append(r)
 
     def return_result(self):
         return self.result
+
+a = make_edit_point(0)
+a.calculate_time_video()
+print(a.return_result())
