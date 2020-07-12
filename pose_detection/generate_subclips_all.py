7fdf908abb18d22579ac130253338e17da62aca4
minjoong507 <albert507@naver.com>
2020-07-12 17:22:58 +0900

    영상길이 안맞는 오류 해결

--- pose_detection/generate_subclips_all.py
+++ pose_detection/generate_subclips_all.py
@@ -18,24 +18,41 @@ class generate_subclips():
             clip = VideoFileClip(video_path)
             clip = clip.subclip(self.start_times[i], clip.duration)
             self.extracted_clips_array.append(clip)
+
+            if self.min_time > clip.duration:
+                audioclip = clip.audio
+                self.min_time = clip.duration
+
         # print(len(extracted_clips_array))
 
 
     def make_subclips(self):
-        for i in range(6):
+        for i in range(33):
             os.mkdir("subclips/" + str(i * 10))
 
         for video_idx in range(len(self.extracted_clips_array)):
-            print("make subclip...")
-            t = 0
-            while t <= 60:
-                # 10 sec.
-                cur_t = t
-                next_t = min(t + 10, self.min_time)
+            try:
+                print("make subclip...")
+                t = 0
+                while t <= self.min_time + 10:
+                    # 10 sec.
+                    cur_t = t
+                    next_t = min(t + 10, self.min_time)
 
-                clip = self.extracted_clips_array[video_idx].subclip(cur_t, next_t)
-                # clip.write_videofile("subclips/" + str(video_idx * 10) + "/" + str(t) + " " + str(video_idx) + ".mp4")
-                t = next_t
+                    clip = self.extracted_clips_array[video_idx].subclip(cur_t, next_t)
+                    clip.write_videofile("subclips/" + str(t) + "/" + str(t) + " " + str(video_idx) + ".mp4")
+                    t = next_t
+            except IndexError:
+                pass
+
 
     def get_extracted_clip_list(self):
         return self.extracted_clips_array
+
+    def run(self):
+        self.edit_video_sink("video/")
+        self.make_subclips()
+
+if __name__ == "__main__":
+    k = generate_subclips()
+    k.run()
